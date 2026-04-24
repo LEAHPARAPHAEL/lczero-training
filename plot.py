b263244fc62a8d650232faf0c6e9898d7f38ae1b
LEAHPARAPHAEL <raphaelmathiot@icloud.com>
2026-04-24 13:01:53 +0200

    plot

--- plot.py
+++ plot.py
@@ -108,7 +108,8 @@ if __name__ == "__main__":
     # 1. List of paths to your models' log files
     MODELS_LOG_PATHS = [
         "/home/raph/leela/logs/8x256x8h.txt",
-        "/home/raph/leela/logs/8x256x8h-crbk.txt"
+        "/home/raph/leela/logs/8x256x8h-crbk.txt",
+        "/home/raph/leela/logs/8x256x8h-2c2r2b2k.txt"
     ]
 
     # 2. List of metrics to plot (Set to None to plot ALL found metrics)
